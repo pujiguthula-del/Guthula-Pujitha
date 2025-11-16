e501cb5ee02355dba41d5e30bf1cc2518aeba2a0
pujiguthula-del <pujiguthula@gmail.com>
2025-11-16 11:10:41 +0530

    Add files via upload

---
+++ Switch case program.cpp
@@ -0,0 +1,32 @@
+#include<stdio.h>
+int main()
+{
+printf ("Enter the value");
+scanf("%d"&n);
+switch(n)
+{
+case 1:printf("Sunday");
+break;
+case 2:printf("Monday");
+break;
+case 3:printf("Tuesday");
+break;
+case 4:printf("wednesday");
+break;
+case 5:printf("Thursday");
+break;
+case 6:printf("Friday");
+break;
+case 7:printf("Saturday");
+break;
+default:printf("Enter the numbers between 1 to 7");
+break;
+}
+return 0;
+}
+
+
+
+
+
+

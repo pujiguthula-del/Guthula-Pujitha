5f9a7b28c9db65e62934a406154d3a1b03ef363e
pujiguthula-del <pujiguthula@gmail.com>
2025-11-17 22:55:20 +0530

    Add files via upload

---
+++ can cross the bridge 1.cpp
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main()
+{
+// Constrained code starts here
+    int x,y,z,r;
+    scanf("%d%d%d",&x,&y,&z);
+
+// Constrained code ends here
+    r=(z-y)/x;
+// Constrained code starts here
+    printf("%d",r);
+
+// Constrained code ends here
+}

91af195a26a50d3a8606a4632ccb898b9ffd7596
pujiguthula-del <pujiguthula@gmail.com>
2025-11-25 10:28:04 +0530

    Add files via upload

---
+++ call by value.cpp
@@ -0,0 +1,16 @@
+#include<stdio.h>
+void swap(int x, int y);
+int main()
+{
+	int a=20,b=30;
+	swap(a,b);
+	printf("a=%d\tb=%d\t",a,b);
+}
+void swap(int x,int y)
+{
+	int temp;
+	temp=x;
+	x=y;
+	y=temp;
+	printf("x=%d\ty=%d\t",x,y);
+}

54dd90e634059d1b9d2d88e58e4f51ec05f261b6
pujiguthula-del <pujiguthula@gmail.com>
2025-11-28 13:25:54 +0530

    Add files via upload

---
+++ spy number.cpp
@@ -0,0 +1,22 @@
+#include<stdio.h>
+int main()
+{
+    int n,r,p=1,s=0;
+    scanf("%d",&n);
+    while (n!=0)
+    {
+        r=n%10;
+        n=n/10;
+        p=p*r;
+        s=s+r;
+    }
+    if (p==s)
+    {
+        printf("Spy Number");
+    }
+    else
+    {
+        printf("Not Spy Number");
+    }
+    return 0;
+}
